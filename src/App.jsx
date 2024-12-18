372683f9eb82ff49b7a50709a2d69962f229708f
jeresc <jeremiastomassrc@gmail.com>
2024-12-18 13:29:06 -0300

    feat: add some basic user interactions

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,63 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import './App.css'
-import PropTypes from 'prop-types' // Import PropTypes for validation
-import { useRef } from 'react'
-
-/** 
-  * Sphere component renders a 3D sphere mesh.
-  *
-  * @param {Object} props The props object.
-  * @param {[number, number, number]} props.position The position of the sphere in the 3D space (x, y, z).
-  * @param {[number, number, number]} props.size The size of the sphere in 3D space (radius, widthSegments, heightSegments).
-  * @param {string} props.color The color of the sphere.
-  * @returns {JSX.Element} The rendered Sphere component.
-  */
-function Sphere({ position, size, color }) {
-
-  const ref = useRef()
-  useFrame((state, delta) => {
-    // Rotate the cube
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += delta
-    ref.current.position.z = Math.sin(state.clock.elapsedTime) + position[2]
-  })
-
-  return <mesh position={position} ref={ref}>
-    <sphereGeometry args={size} />
-    <meshStandardMaterial color={color} />
-  </mesh>
-}
-
-Sphere.propTypes = {
-  position: PropTypes.arrayOf(PropTypes.number).isRequired,
-  size: PropTypes.arrayOf(PropTypes.number).isRequired,
-  color: PropTypes.string.isRequired,
-}
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./App.css";
+import PropTypes from "prop-types"; // Import PropTypes for validation
+import { useRef } from "react";
+import { useState } from "react";
 
 /**
-  * Torus component renders a 3D torus mesh.
-  *
-  * @param {Object} props The props object.
-  * @param {[number, number, number]} props.position The position of the torus in the 3D space (x, y, z).
-  * @param {[number, number, number]} props.size The size of the torus in 3D space (radius, tube, radialSegments, tubularSegments).
-  * @param {string} props.color The color of the torus.
-  * @returns {JSX.Element} The rendered Torus component.
-  */
+ * Torus component renders a 3D torus mesh.
+ *
+ * @param {Object} props The props object.
+ * @param {[number, number, number]} props.position The position of the torus in the 3D space (x, y, z).
+ * @param {[number, number, number]} props.size The size of the torus in 3D space (radius, tube, radialSegments, tubularSegments).
+ * @param {[string, string]} props.color The color of the torus.
+ * @returns {JSX.Element} The rendered Torus component.
+ */
 function Torus({ position, size, color }) {
+  const ref = useRef();
 
-  const ref = useRef()
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
   useFrame((state, delta) => {
-    // Rotate the cube
-    ref.current.rotation.x += delta
-    ref.current.rotation.y += delta
-    ref.current.position.z = Math.sin(state.clock.elapsedTime) + position[2]
-  })
+    const speed = isHovered ? 1 : 0.2;
+    ref.current.rotation.y += delta * speed;
+  });
 
-  return <mesh position={position} ref={ref}>
-    <torusKnotGeometry args={size} />
-    <meshStandardMaterial color={color} />
-  </mesh>
+  return (
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={() => setIsHovered(false)}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? [1.2, 1.2, 1.2] : [1, 1, 1]}
+    >
+      <torusKnotGeometry args={size} />
+      <meshStandardMaterial color={isHovered ? color[0] : color[1]} />
+    </mesh>
+  );
 }
 
 Torus.propTypes = {
   position: PropTypes.arrayOf(PropTypes.number).isRequired,
   size: PropTypes.arrayOf(PropTypes.number).isRequired,
   color: PropTypes.string.isRequired,
-}
-
+};
 
 function App() {
-
   return (
     <Canvas>
       <directionalLight position={[2, 2, 0]} />
       <directionalLight position={[-2, -1, 0]} />
       <ambientLight intensity={0.8} />
-      <Sphere position={[-1, -1, 0]} size={[1, 12, 12]} color="red" />
-      <Torus position={[1, 2, 0]} size={[1, 0.4, 32, 32]} color="skyBlue" />
+      <Torus
+        position={[0, 0, 0]}
+        size={[1, 0.4, 32, 32]}
+        color={["skyBlue", "red"]}
+      />
     </Canvas>
-  )
+  );
 }
 
-export default App
+export default App;
